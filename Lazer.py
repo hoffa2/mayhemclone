3ce2ad174224aa699853bfc7f12e2567b398f66b
Øystein Tveito <ystein@Tveito-Laptop.(none)>
2014-04-01 10:31:30 +0200

    tveito

---
+++ Lazer.py
@@ -0,0 +1,22 @@
+class Lazer(pygame.sprite.Sprite):
+	def __init__(self, pos, vel):
+		pygame.sprite.Sprite.__init__(self)
+		self.pos = pos
+		self.vel = vel.normalized()
+		
+		self.Cimage = pygame.Surface((10, 20))
+		self.Cimage = self.Cimage.convert_alpha()
+		pygame.draw.ellipse(self.Cimage, (255,0, 0), [0, 0, 10, 20], 2)
+		
+		
+		self.image = self.Cimage #pygame.transform.rotate(self.Cimage, math.degrees(self.vel.angle()))
+		self.rect = self.image.get_rect()
+		self.rect.centerx = pos.x
+		self.rect.centery = pos.y
+
+	def update(self):
+		self.pos += self.vel * 20
+		self.image = pygame.transform.rotate(self.Cimage, math.degrees(self.vel.angle()) + 180)
+		self.rect = self.image.get_rect()
+		self.rect.centerx = self.pos.x
+		self.rect.centery = self.pos.y
--- vector.py
+++ vector.py
@@ -1,82 +1,81 @@
-import math
-class Vector2D(object):
-    """ Implements a two dimensional vector. """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Vector(object):
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		
+	def __repr__(self):
+		return "Vector(%s, %s)" % (self.x, self.y)
+			
+	def __add__(self, b):
+		""" Addition. Returns a new vector. """
+		return Vector(self.x + b.x, self.y + b.y)
 
-    def __repr__(self):
-        return "Vector(%s, %s)" % (self.x, self.y)
+	def __sub__(self, b):
+		""" Subtraction. Returns a new vector. """
+		return Vector(self.x - b.x, self.y - b.y)
 
-    def __add__(self, b):
-        """ Addition. Returns a new vector. """
-        return Vector2D(self.x + b.x, self.y + b.y)
+	def __mul__(self, b):
+		""" Multiplication by a scalar
+		
+		Note that the scalar must be to the right.
+		
+		"""
+		try:
+			b = float(b)
+			return Vector(self.x * b, self.y * b)
+		except ValueError:
+			print "Oops! Right value must be a float"
+			raise
 
-    def __sub__(self, b):
-        """ Subtraction. Returns a new vector. """
-        return Vector2D(self.x - b.x, self.y - b.y)
+	def magnitude(self):
+		""" Returns the magnitude of the vector. """
+		return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def __getitem__(self, key):
-        if key == 0:
-            return self.x
-        elif key == 1:
-            return self.y
-        else:
-            raise IndexError("Invalid subscript "+str(key)+" to Vec2d")
+	def normalized(self):
+		""" Returns a new vector with the same direction but magnitude 1. """
+		try:
+			m = self.magnitude()
+			return self / m
+		except ZeroDivisionError:
+			print "Oops! Cannot normalize a zero-vector"
+			raise
 
-    def __setitem__(self, key, value):
-        if key == 0:
-            self.x = value
-        elif key == 1:
-            self.y = value
-        else:
-            raise IndexError("Invalid subscript "+str(key)+" to Vec2d")
+	def copy(self):
+		""" Returns a copy of the vector. """
+		return Vector(self.x, self.y)
+	def __div__(self, b):
+		""" Dividing a vector by b """
+		try:
+			if b == 0:
+				return Vector(0,0)
+			b = float(b)
+			return Vector(self.x/b, self.y/b)
+		except ValueError:
+			print "Oops! Right value must be a float"
+			raise
 
+	def __getitem__(self, key):
+		""" Get item at index "key" """
+		if key == 0:
+			return int(self.x)
+		elif key == 1:
+			return int(self.y)
+		else:
+			print "Oops! Right value must be a float"
+			raise
 
-    def __abs__(self):
-        return Vector2D(abs(self.x), abs(self.y))
+	def __len__(self):
+		""" Get lenght of vector """
+		return 2
 
-    def __div__(self, b):
+	def diff(self, b):
+		""" Get distance between two vectors """
+		return (self - b).magnitude()
 
-        try:
-            b = float(b)
-            return Vector2D(self.x / b, self.y / b)
-        except ValueError:
-            print "Oops! Right value must be a float"
-            raise
+	def angle(self):
+		""" Calculates the angle of the vector (radians) """ 
+		return math.atan2(self.x, self.y)
 
-    def __mul__(self, b):
-        """ Multiplication by a scalar
-
-        Note that the scalar must be to the right.
-
-        """
-        try:
-            b = float(b)
-            return Vector2D(self.x * b, self.y * b)
-        except ValueError:
-            print "Oops! Right value must be a float"
-            raise
-
-    def __eq__(self, b):
-        if (self.x == b) & (self.y == b):
-            return True
-        else:
-            return False
-
-    def magnitude(self):
-        """ Returns the magnitude of the vector. """
-        return math.sqrt(self.x ** 2 + self.y ** 2)
-
-    def normalized(self):
-        """ Returns a new vector with the same direction but magnitude 1. """
-        try:
-            m = self.magnitude()
-            return Vector2D(self.x / m, self.y / m) * 2
-        except ZeroDivisionError:
-            print "Oops! Cannot normalize a zero-vector"
-            raise
-
-    def copy(self):
-        """ Returns a copy of the vector. """
-        return Vector2D(self.x, self.y)
+	def rotate(self, theta):
+		""" Rotatates the vector "theta" degrees (radians) """
+		return Vector(self.x * math.cos(theta) - self.y * math.sin(theta), self.x * math.sin(theta) + self.y * math.cos(theta))
